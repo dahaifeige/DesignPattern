5fadf93392efede8c31b25d8edaea35116e3ea71
wangfei <904133298@qq.com>
2022-09-16 23:16:36 +0800

    1. DecoratorPattern 2. ObserverPattern 3. StrategyPattern

---
+++ ObserverPattern/main.cpp
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <iostream>
+#include <list>
+using namespace std;
+// 观察者模式: 定义了对象之间的一对多依赖，当一个对象改变状态时，它的所有依赖者都会收到通知并自动更新。
+// 原则：
+// 1. 为了交互对象之间的松耦合而努力；
+
+class Observer {
+public:
+    virtual void update(float temp, float humidity, float pressure) = 0;
+};
+
+class Subject {
+public:
+    virtual void registerObserver(Observer*) = 0;
+    virtual void removeObserver(Observer*) = 0;
+    virtual void notifyObserver() = 0;
+};
+
+class DisplayElement {
+    virtual void display() = 0;
+};
+
+class WeatherData : public Subject {
+public:
+    void registerObserver(Observer *o) override {
+        observers.push_back(o);
+    }
+    void removeObserver(Observer *o) override {
+        observers.remove(o);
+    }
+    void notifyObserver() override {
+        for (auto o : observers) {
+            o->update(temperature, humidity, pressure);
+        }
+    }
+    void measurementsChanged() {
+        notifyObserver();
+    }
+    void setMeasurements(float temp, float humidity, float pressure) {
+        this->temperature = temp;
+        this->humidity = humidity;
+        this->pressure = pressure;
+        measurementsChanged();
+    }
+private:
+    list<Observer *> observers;
+    float temperature;
+    float humidity;
+    float pressure;
+};
+
+
+class CurrentConditionsDisplay : public Observer, public DisplayElement {
+public: 
+    CurrentConditionsDisplay(Subject *weatherData) {
+        this->weatherData = weatherData;
+        this->weatherData->registerObserver(this);
+    }
+    void update(float temp, float humidity, float pressure) override {
+        this->temperature = temp;
+        this->humidity = humidity;
+        display();
+    }
+    void display() override {
+        cout << "temperature:" << temperature << " humidity:" << humidity << endl;
+    }
+private:
+    float temperature;
+    float humidity;
+    Subject *weatherData;
+
+};
+
+
+
+
+int main(int argc, char ** argv)
+{
+    WeatherData *weatherData = new WeatherData;
+    Observer *currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+    weatherData->setMeasurements(30, 34, 44);
+    weatherData->setMeasurements(23, 33, 44);
+    weatherData->setMeasurements(23, 22, 44);
+
+    return 0;
+}
---
+++ StrategyPattern/main.cpp
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <iostream>
+
+// 策略模式: 定义了算法簇，分别封装起来，让它们之间可以相互替换，此模式让算法的变化独立于使用算法的客户。
+// 原则：
+// 1. 封装变化，找出变化，独立出来，不要和不需要变化的代码混在一起；
+// 2. 针对接口编程，而不是针对实现变成；
+// 3. 多用组合，少用继承；
+
+using namespace std;
+
+class FlyBehavior {
+public:
+    virtual void fly() = 0;
+};
+
+class FlyWithWings : public FlyBehavior {
+public:
+    virtual void fly() {
+        cout << "i can fly" << endl;
+    }
+};
+
+class FlyNoWay : public FlyBehavior {
+public:
+    virtual void fly() {
+        cout << "i can't fly" << endl;
+    }
+};
+
+class QuackBehavior {
+public:
+    virtual void quack() = 0;
+};
+
+class Quack : public QuackBehavior {
+public:
+    virtual void quack() {
+        cout << "i quack" << endl;
+    }
+};
+
+class MuteQuack : public QuackBehavior {
+public:
+    virtual void quack() {
+        cout << "i silence" << endl;
+    }
+};
+
+class Duck {
+public:
+    void setFlyBehavior(FlyBehavior *fb)
+    {
+        flyBehavior = fb;
+    }
+    void setQuackBevior(QuackBehavior *qb) 
+    {
+        quackBehavior = qb;
+    }
+
+    void performFly()
+    {
+        flyBehavior->fly();
+    }
+    void performQuack()
+    {
+        quackBehavior->quack();
+    }
+protected:
+    FlyBehavior *flyBehavior;
+    QuackBehavior *quackBehavior;
+};
+
+class MallardDuck : public Duck {
+public:
+  MallardDuck() {
+    flyBehavior = new FlyWithWings;
+    quackBehavior = new Quack;
+  }
+  
+};
+
+
+int main(int argc, char ** argv)
+{
+    Duck * duck = new MallardDuck;
+    duck->performFly();
+    duck->performQuack();
+    duck->setFlyBehavior(new FlyNoWay);
+    duck->performFly();
+    return 0;
+}
